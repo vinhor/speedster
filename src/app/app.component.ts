0f2bbfc7d5f398dffc936ce1fbfacfdbb266102d
Vincent Horecký <vincent.horecky@seznam.cz>
2025-02-26 18:36:22 +0100

    fixed link in app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import { LinkComponent } from './link/link.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
